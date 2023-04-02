859d52a9396e78ee18a647f1712d23dd4c83afd4
Jiwoo So <58384366+jiwooproity@users.noreply.github.com>
2023-04-02 16:31:54 +0900

    webpack 기본 설정 파일 작성 및 빌드 파일 index.html 의존성 추가 / 테스트 완료

--- javascript/webpack/lecture-learn-project/src/app.js
+++ javascript/webpack/lecture-learn-project/src/app.js
@@ -1,1 +1,3 @@
-math.sum(1, 2);
+import * as math from "./math.js";
+
+console.log(math.sum(1, 2));
--- javascript/webpack/lecture-learn-project/src/math.js
+++ javascript/webpack/lecture-learn-project/src/math.js
@@ -1,9 +1,13 @@
-var math = math || {};
+// var math = math || {};
 
-(function() {
-    function sum(a, b) {
-        console.log(`요청 값의 합 : ${a + b}`)
-    }
+// (function() {
+//     function sum(a, b) {
+//         console.log(`요청 값의 합 : ${a + b}`)
+//     }
 
-    math.sum = sum;
-})();
+//     math.sum = sum;
+// })();
+
+export function sum(a, b) {
+    return a + b;
+}
