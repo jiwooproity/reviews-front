b5b4e580856f526a22d58ddaa2bf6e0bdde459fd
jiwooproity <jiwooproity@naver.com>
2023-07-23 11:05:01 +0900

    practice: 발사되는 폭죽 만들기

--- javascript/canvas/firework/index.js
+++ javascript/canvas/firework/index.js
@@ -1,76 +1,150 @@
-const canvas = document.getElementById('canvas');
-const context = canvas.getContext('2d');
+const canvas = document.getElementById("canvas");
+const context = canvas.getContext("2d");
+
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-let hue = 0;
+const fireworkObjectArr = [];
 
-const particleArr = [];
+class Firework {
+  constructor(x, y, color, boomSize) {
+    this.x = x;
+    this.y = y;
+    this.color = color;
+    this.opacity = 1;
+    this.size = Math.random() * 2 + 1;
+    this.speedX = Math.random() * boomSize - boomSize / 2;
+    this.speedY = Math.random() * boomSize - boomSize / 2;
+  }
 
-let mouse = {
-    x: null,
-    y: null
+  update() {
+    if (this.opacity > 0.01) this.opacity -= 0.015;
+    if (this.size > 0.8) {
+      this.x += this.speedX;
+      this.y += this.speedY;
+      this.size -= 0.05;
+    } else {
+      this.y += 1;
+    }
+  }
+
+  draw() {
+    context.fillStyle = this.color + ", " + this.opacity + ")";
+    context.beginPath();
+    context.arc(this.x, this.y, this.size, 0, Math.PI * 2);
+    context.fill();
+    context.closePath();
+  }
 }
 
-class Particle {
-    constructor() {
-        this.x = mouse.x;
-        this.y = mouse.y;
-        this.color = "hsl(" + hue +  ", 100%, 50%, 0.8)";
-        this.size = Math.random() * 3  + 1;
-        this.speedX = Math.random() * 7 - 3.5;
-        this.speedY = Math.random() * 7 - 3.5;
-    }
+class FireworkObject {
+  constructor() {
+    this.x = Math.random() * window.innerWidth + 1;
+    this.y = Math.random() * 1000 + window.innerHeight;
+    this.rotate = Math.random() * 3 - 1.5;
+    this.timing = (Math.random() * window.innerHeight + 500) / 2;
+    this.color = "hsl(" + Math.random() * 1000 + 1 + ", 100%, 50%";
+    this.objWidth = 1;
+    this.objHeight = 5;
+    this.speedY = 10;
+    this.firework = [];
+  }
 
-    update() {
-        this.x += this.speedX;
-        this.y += this.speedY;
-        if (this.size > 0.2) this.size -= 0.05;
-    }
+  update() {
+    this.y -= 10;
+  }
 
-    draw() {
-        context.fillStyle = this.color;
-        context.beginPath();
-        context.arc(this.x, this.y, this.size, 0, Math.PI * 2);
-        context.fill();
-        context.closePath();
+  draw() {
+    if (this.y < this.timing) {
+      let boomSize = Math.random() * 15 + 1;
+      for (let i = 0; i < 50; i++) {
+        this.firework.push(new Firework(this.x, this.y, this.color, boomSize));
+      }
+
+      for (let i = 0; i < 100; i++) {
+        this.firework.push(
+          new Firework(this.x, this.y, "rgb(255, 255, 255", boomSize)
+        );
+      }
+    } else {
+      context.beginPath();
+      context.fillStyle = this.color + ", 0.8)";
+      context.rect(this.x, this.y, this.objWidth, this.objHeight);
+      context.fill();
+      context.fillStyle = this.color + ", 0.7)";
+      context.rect(this.x, this.y + 10, this.objWidth, this.objHeight);
+      context.fill();
+      context.fillStyle = this.color + ", 0.6)";
+      context.rect(this.x, this.y + 20, this.objWidth, this.objHeight);
+      context.fill();
+      context.fillStyle = this.color + ", 0.5)";
+      context.rect(this.x, this.y + 30, this.objWidth, this.objHeight);
+      context.fill();
+      context.fillStyle = this.color + ", 0.4)";
+      context.rect(this.x, this.y + 40, this.objWidth, this.objHeight);
+      context.fill();
+      context.fillStyle = this.color + ", 0.3)";
+      context.rect(this.x, this.y + 60, this.objWidth, this.objHeight);
+      context.fill();
+      context.fillStyle = this.color + ", 0.2)";
+      context.rect(this.x, this.y + 90, this.objWidth, this.objHeight);
+      context.fill();
+      context.fillStyle = this.color + ", 0.1)";
+      context.rect(this.x, this.y + 120, this.objWidth, this.objHeight);
+      context.fill();
+      context.closePath();
     }
+  }
 }
 
-const onClick = (e) => {
-    mouse.x = e.x;
-    mouse.y = e.y;
+const onHandleDraw = () => {
+  if (fireworkObjectArr.length === 0) return;
 
-    for(let i = 0; i < 100; i++) {
-        particleArr.push(new Particle());
-    }
-}
-
-const onDraw = () => {
-    for(let i = 0; i < particleArr.length; i++) {
-        if(particleArr[i].size > 0) {
-            particleArr[i].update();
-            particleArr[i].draw();
-        } else {
-            particleArr = [];
+  for (let i = 0; i < fireworkObjectArr.length; i++) {
+    if (fireworkObjectArr[i].y > fireworkObjectArr[i].timing) {
+      fireworkObjectArr[i].update();
+      fireworkObjectArr[i].draw();
+    } else {
+      if (fireworkObjectArr[i].firework.length !== 0) {
+        for (let j = 0; j < fireworkObjectArr[i].firework.length; j++) {
+          if (fireworkObjectArr[i].firework[j].opacity > 0.02) {
+            fireworkObjectArr[i].firework[j].update();
+            fireworkObjectArr[i].firework[j].draw();
+          } else {
+            fireworkObjectArr[i].firework.splice(j, 1);
+            j--;
+          }
         }
+      }
     }
-}
+  }
+};
 
-const canvasAnimation = () => {
-    context.clearRect(0, 0, canvas.width, canvas.height);
-    onDraw();
-    hue += 0.5;
-    requestAnimationFrame(canvasAnimation);
-}
+const onHandleRequestAnimation = () => {
+  context.clearRect(0, 0, canvas.width, canvas.height);
+  onHandleDraw();
+  requestAnimationFrame(onHandleRequestAnimation);
+};
 
-canvas.addEventListener('click', onClick);
+const onHandleResize = () => {
+  canvas.width = window.innerWidth;
+  canvas.height = widnow.innerHeight;
+};
 
-const onResize = () => {
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
-}
+const onHandleAudio = () => {
+  const audio = document.getElementById("audio");
+  audio.muted = false;
+  audio.play();
+};
 
-window.addEventListener('resize', onResize);
+const onHandleObjectInit = () => {
+  fireworkObjectArr.push(new FireworkObject());
+};
 
-canvasAnimation();
+setInterval(() => {
+  onHandleObjectInit();
+}, 200);
+
+onHandleRequestAnimation();
+document.addEventListener("DOMContentLoaded", onHandleAudio);
+window.addEventListener("resize", onHandleResize);
