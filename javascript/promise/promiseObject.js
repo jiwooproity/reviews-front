fdf0d79dfb92a06d267dd525a1b981ac0be20f60
jiwooproity <jiwooproity@naver.com>
2023-09-28 22:49:26 +0900

    test: check object get key

--- javascript/promise/promiseObject.js
+++ javascript/promise/promiseObject.js
@@ -1,19 +1,35 @@
 const promise = new Promise((resolve, reject) => {
-    setTimeout(() => {
-        resolve({
-            name: "So Ji Woo",
-            job: 'programmar',
-            age: 24,
-        })
-    }, 500);
+  setTimeout(() => {
+    resolve({
+      name: "So Ji Woo",
+      job: "programmar",
+      age: 24,
+    });
+  }, 500);
 });
 
 const onLoad = async () => {
-    const data = await promise;
-    
-    for(let key in data) {
-        console.log(`${key} : ${data[key]}`)
-    }
-}
+  const data = await promise;
+
+  for (let key in data) {
+    console.log(`${key} : ${data[key]}`);
+  }
+};
 
 onLoad();
+
+const promise2 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve({
+      name: "data",
+    });
+  }, 500);
+});
+
+const onLoadName = async () => {
+  const { name } = await promise2;
+
+  console.log(`He's name ${name}`);
+};
+
+onLoadName();
