62f6f7fe20f63267ed3b31595e127f47e53f6048
jiwooproity <jiwooproity@naver.com>
2023-10-17 23:52:01 +0900

    test: generator check review

--- javascript/generator/review/yieldReview01.js
+++ javascript/generator/review/yieldReview01.js
@@ -16,3 +16,11 @@ console.log(yiledData.next().value);
 console.log(yiledData.next().value);
 console.log(yiledData.next().value);
 console.log(yiledData.next().value);
+
+function* yieldRandom() {
+  const arr = Array.from({ length: 10 }).fill(0);
+  yield* arr.map((item) => Math.floor(Math.random() * 10));
+}
+
+const randomData = yieldRandom();
+console.log(randomData.next().value);
--- javascript/this/function.js
+++ javascript/this/function.js
@@ -1,12 +1,12 @@
 function data() {
-    this.name = 1;
-    this.show = function() {
-        function d() {
-            console.log(this);
-        }
-
-        d();
+  this.name = 1;
+  this.show = function () {
+    function d() {
+      console.log(this);
     }
+
+    d();
+  };
 }
 
 const test = new data();
