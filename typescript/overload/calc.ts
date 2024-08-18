c2033c6eaf302c544fbd93c2700450d21b6fb499
Jiwoo So <58384366+jiwooproity@users.noreply.github.com>
2024-08-18 16:33:19 +0900

    Add: Use overload of typescript to get add, multiply result

---
+++ typescript/overload/calc.ts
@@ -0,0 +1,16 @@
+function calc(a: number, b: number): number;
+function calc(a: number, b: number, c: number): number;
+
+function calc(a: number, b: number, c?: number): number {
+  if (c) {
+    return (a + b) * c;
+  }
+
+  return a + b;
+}
+
+(() => {
+  const sum = calc(3, 2);
+  const sumAndMulti = calc(2, 2, 2);
+  console.log(sum, sumAndMulti); // 5, 8
+})();
